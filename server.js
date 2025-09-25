2cc0cc5fa8fd90e5549cadb27d26cd58dd8fb359
TheRedCat22 <theredcat222@gmail.com>
2025-09-25 17:24:03 +0200

    Public Folder
    - Talwind Added

--- server.js
+++ server.js
@@ -1,29 +1,19 @@
-// Import express
 const express = require("express");
 const path = require("path");
 
-// Initialize app
 const app = express();
 
-// Set EJS as the view engine
 app.set("view engine", "ejs");
-
-// Set views folder
+app.use(express.static('public'));
 app.set("views", path.join(__dirname, "views"));
-
-// Middleware (optional)
-app.use(express.urlencoded({ extended: true })); // for form submissions
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
 
-// Example route
 app.get("/", (req, res) => {
-  res.render("index", { title: "Home Page", message: "Hello from EJS!" });
+  res.render("index");
 });
 
-// Set port
-const PORT = process.env.PORT || 3000;
-
-// Start server
+const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
